922c0bb0f56ac152282dae7cb051b2eece63a6b1
Saylaubay <saylaubaybekmurzaev@gmail.com>
2024-04-05 12:08:32 +0500

    #2

---
+++ src/Adam.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Saylaubay\PhpTest;
+
+
+class Adam
+{
+    public function getFam()
+    {
+        return "Gates";
+    }
+    public function getJil()
+    {
+        return "1994 jili";
+    }
+}
